821fca7f62399d6c3195a9adc29f2229200cc4bf
magdominguezm <magdominguezm@gmail.com>
2021-05-04 16:37:32 +0200

    Clase Cambio1

--- prueba1/src/cambio1/Cambio1.java
+++ prueba1/src/cambio1/Cambio1.java
@@ -2,6 +2,6 @@ package cambio1;
 
 public class Cambio1 {
 	public static void main(String[] args) {
-		System.out.println("Pruebas Git. Clase Cambio1");
+		System.out.println("Pruebas de versiones Git. Clase Cambio1");
 	}
 }
